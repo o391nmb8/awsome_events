4c99dc73af2e7573267441c69477d82a6492e551
masa <o391nmb8@gmail.com>
2021-10-06 15:35:38 +0900

    8-1 ファイルアップロード機能を作る、8-2 gemで機能拡張をする

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,11 @@
 class Event < ApplicationRecord
+  has_one_attached :image, dependent: false
   has_many :tickets, dependent: :destroy
   belongs_to :owner, class_name: "User"
   
+  validates :image, content_type: [:png, :jpg, :jpeg], 
+    size: { less_than_or_equal_to: 10.megabytes }, 
+    dimension: { width: { max: 2000 }, height: { max: 2000 } }
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
   validates :content, length: { maximum: 2000 }, presence: true
@@ -9,12 +13,20 @@ class Event < ApplicationRecord
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
 
+  attr_accessor :remove_image
+
+  before_save :remove_image_if_user_accept
+
   def created_by?(user)
     return false unless user
     owner_id == user.id
   end
 
   private
+
+  def remove_image_if_user_accept
+    self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+  end
 
   def start_at_should_be_before_end_at
     return unless start_at && end_at
